7070a85e79ab572090f0efa1dfaba6dfcb85d04c
AntonioTorralba <atorralba96@gmail.com>
2019-06-04 18:33:18 +0200

    Controladores, filtros y formularios

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
-    Vista detalle producto {{ $id }}
+    <div class="row">
+
+        <div class="col-sm-4">
+
+            <img src="https://picsum.photos/200/300/?random" style="height:500px"/>
+
+        </div>
+        <div class="col-sm-8">
+
+            <h1 style="min-height:45px;margin:5px 0 10px 0">
+                {{$producto[0]}}
+            </h1>
+            <h4 style="min-height:45px;margin:5px 0 10px 0">
+                {{$producto[1]}}
+            </h4>
+            <p><b>Estado: </b>Producto actualmente comprado</p>
+            <button type="button" class="btn btn-danger">Pendiente de Compra</button>
+            <button type="button" class="btn btn-warning">Editar Producto</button>
+            <button type="button" class="btn btn-default">Volver al Listado de Productos</button>
+
+        </div>
+    </div>
 
 @stop
